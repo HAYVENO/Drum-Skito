86f2de9cf70e496ceb942fd51ce6af3fa1e91f71
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-04-19 03:07:06 +0100

    initial

---
+++ contstructionFunction.js
@@ -0,0 +1,12 @@
+function TopBoy(Name, Age, Class, Workpermit, Gender, language) {
+  this.name = Name;
+  this.age = Age;
+  this.class = Class;
+  this.Workpermit = Workpermit;
+  this.Gender = Gender;
+  this.language = language;
+}
+
+var TopBoy1 = new TopBoy("Adeleke", 12, "Graduate", "Yes", "Male", [
+  "English, Arabic",
+]);
